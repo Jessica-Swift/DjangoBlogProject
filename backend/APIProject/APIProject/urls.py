4eab631fb18c9fdb07a154807017a782da02c330
Jessica Kim <jessica.kim@outlook.de>
2022-08-19 23:00:02 +0200

    Django, creates Models

--- backend/APIProject/APIProject/urls.py
+++ backend/APIProject/APIProject/urls.py
@@ -1,7 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# 30:34
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('api.urls')),
 ]
