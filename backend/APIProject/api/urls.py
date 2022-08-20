ed1800c38be9724ec79be6dfe8a6b725ca2174ef
Jessica Kim <jessica.kim@outlook.de>
2022-08-20 13:42:32 +0200

    Django, Get and Post

--- backend/APIProject/api/urls.py
+++ backend/APIProject/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import article_details, article_list
 
 urlpatterns = [
-    path('', Index),
+    path('articles/', article_list),
+    path('articles/<int:pk>/', article_details),
 ]
