2df9a017b4c468e2f4c9b3af84fe1b3f0a6933c3
Jessica Kim <jessica.kim@outlook.de>
2022-08-21 01:54:55 +0200

    Django, Authentication and Token

--- backend/APIProject/APIProject/settings.py
+++ backend/APIProject/APIProject/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', 
+    'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/APIProject/api/views.py
+++ backend/APIProject/api/views.py
@@ -1,4 +1,6 @@
+from cgitb import lookup
 from os import stat
+from urllib import request
 from django.shortcuts import render, HttpResponse
 from .models import Article
 from .serializers import ArticleSerializer
@@ -8,48 +10,102 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
-class ArticleList(APIView):
-    def get(self, request):
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = TokenAuthentication
+
+
+'''
+class ArticleViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+'''
+
+'''
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def create(self, request):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
-
-class ArticleDetails(APIView):
-    def get_object(self, id):
-        try:
-            return Article.objects.get(id=id)
-
-        except Article.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, id):
-        article = self.get_object(id)
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
 
-    def put(self, request, id):
-        article = self.get_object(id)
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+
         serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, id):
-        article = self.get_object(id)
+    def destroy(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
 
+'''
+class ArticleList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class ArticleDetails(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    lookup_field = 'id'
+
+    def get(self, request, id):
+        return self.retrieve(request, id=id)
+
+    def put(self, request, id):
+        return self.update(request, id=id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id=id)
+'''
 
 '''
 @api_view(['GET', 'POST'])
