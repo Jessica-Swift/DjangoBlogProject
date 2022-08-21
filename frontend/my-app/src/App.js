e196f8038ddda048a1d3c99df0c9eff37b4dae66
Jessica Kim <jessica.kim@outlook.de>
2022-08-22 01:06:21 +0200

    React,  learning hooks

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,30 +8,26 @@ import "./App.css";
 // import Form from "./components/Form";
 // import MyFragment from "./components/MyFragment";
 import ComponentA from "./components/ComponentA";
+import Counter from "./components/Counter";
+import CounterHook from "./components/CounterHook";
 
 function App() {
+  function clicked() {
+    alert("Inside App.js");
+  }
+
   return (
     <div className="container">
+      {/* <Name />
+      <Example names={["Python", "Java", "JavaScript", "C#"]} />
+      <Example2 names={["React", "React Native", "Django"]} />
+      <Form />  
+      <MyFragment /> */}
       <ComponentA />
+      <Counter />
+      <CounterHook />
     </div>
   );
 }
-
-// function App() {
-//   function clicked() {
-//     alert("Inside App.js");
-//   }
-
-//   return (
-//     <div className="container">
-//       {/* <Name />
-//       <Example names={["Python", "Java", "JavaScript", "C#"]} />
-//       <Example2 names={["React", "React Native", "Django"]} />
-//       <Form />   */}
-
-//       <MyFragment />
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ frontend/my-app/src/components/CounterHook.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+
+function CounterHook() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("This is a text");
+  const [info, setInfo] = useState({ name: "", email: "" });
+  const [articles, setArticles] = useState([
+    "Article One",
+    "Article Two",
+    "Article Three",
+  ]);
+
+  const addArticle = () => {
+    setArticles([...articles, "New Article"]);
+  };
+
+  // useEffect(() => {
+  //   console.log("useEffec is called");
+  // }, [count, text]);
+
+  useEffect(() => {
+    console.log("useEffec is called");
+    document.title = `You have clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <h2> {count}</h2>
+
+      <button onClick={() => setCount(count + 1)} className="btn btn-primary">
+        Click
+      </button>
+
+      <h2>{text}</h2>
+
+      <button
+        onClick={() => setText("The text is changed")}
+        className="btn btn-success"
+      >
+        Change text
+      </button>
+
+      <br />
+      <br />
+      <input
+        type="text"
+        className="form-control"
+        value={info.name}
+        onChange={(e) => setInfo({ ...info, name: e.target.value })}
+      />
+      <input
+        type="text"
+        className="form-control"
+        value={info.email}
+        onChange={(e) => setInfo({ ...info, email: e.target.value })}
+      />
+
+      <h2>Name: {info.name} </h2>
+      <h2>Email: {info.email} </h2>
+
+      <br />
+      <br />
+
+      {articles.map((article) => {
+        return <h2 key={article}>{article}</h2>;
+      })}
+
+      <button onClick={addArticle} className="btn btn-primary">
+        Add Article
+      </button>
+
+      <button onClick={() => setCount(count + 1)} className="btn btn-primary">
+        Change Title
+      </button>
+    </div>
+  );
+}
+
+export default CounterHook;
