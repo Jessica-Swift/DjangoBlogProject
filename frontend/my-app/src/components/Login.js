a09ca603240e302abce0db1e747ba0d4f172c4c3
Jessica Kim <jessica.kim@outlook.de>
2022-08-25 10:53:54 +0200

    Created login

---
+++ frontend/my-app/src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import APIService from "../APIService";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const loginBtn = () => {
+    APIService.LoginUser({ username, password })
+      .then((resp) => console.log(resp.token))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="App">
+      <br />
+      <br />
+      <h1>Please Login</h1>
+      <br />
+      <br />
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          placeholder="Please Enter Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="Please Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button onClick={loginBtn} className="btn btn-primary">
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -4,8 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
 // import { Routes, BrowserRouter } from "react-router-dom";
 // import Form from "./components/Form";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route exact path="/" component={Login} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // function Router() {
 //   return (
@@ -16,10 +29,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //   );
 // }
 
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/articles" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <Routes exact path="/" component={App} /> */}
+    <Router />
   </React.StrictMode>
 );
